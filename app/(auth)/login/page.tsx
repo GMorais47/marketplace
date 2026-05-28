2dd8e0b63911128b8cbacf37336c82a186522976
Gustavo Morais <gustavomorais47.gm@gmail.com>
2026-05-27 22:24:18 -0300

    feat: paginas de registro e registro de vendedor(seller)

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -18,14 +18,14 @@ export default function Page() {
         console.log(email?.toString(), password?.toString())
     }
 
-    return <main className="flex items-center justify-center flex-1">
-        <Card>
+    return (
+        <>
             <form onSubmit={onSubmit} className="flex flex-col gap-2.5">
                 <Input id="email" name="email" required type="email" label="E-mail" placeholder="Ex: seu-email@dominio.com" />
                 <Input id="password" name="password" required type="password" label="Senha" placeholder="Ex: ••••••••" />
                 <Button type="submit">Entrar</Button>
             </form>
             <p className="mt-4 text-sm font-light">Ainda não possui uma conta? <Link href={"/register"} className="text-[#1C4694] font-semibold">Criar conta!</Link></p>
-        </Card>
-    </main>
+        </>
+    )
 }
