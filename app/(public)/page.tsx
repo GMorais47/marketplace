656fe457055c99e1840af56d8d8a6737c406583c
Gustavo Morais <gustavomorais47.gm@gmail.com>
2026-06-15 22:22:11 -0300

    chore: listagem inicial dos produtos

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,5 +1,51 @@
+import { FaCartPlus } from "react-icons/fa";
+import { Card } from "../components/card";
+import { CATEGORIES } from "../mocks/categories";
+import { PRODUCTS } from "../mocks/products";
+
 export default function Page() {
   return (
-   <></>
+    <>
+      <section>
+        <h2>Mais Vendidos</h2>
+        <ul className="border w-full p-2 flex flex-row gap-2 items-center">
+          {
+            PRODUCTS.map((prd) => (
+              <li key={`best-sellers-prd-${prd.id}`}>
+                <Card>
+                  <div className="border p-2"></div>
+                  <div>
+                    <div className="text-[10px]">{prd.name}</div>
+                    <div className="text-[8px]">
+                      {
+                        CATEGORIES
+                          .find(ct => ct.id === prd.categoryID)?.name || "Não Identificado"
+                      }
+                    </div>
+                    <div className="text-[8px]">
+                      {
+                        CATEGORIES // Alterar futuramente
+                          .find(ct => ct.id === prd.sellerID)?.name || "Não Identificado"
+                      }
+                    </div>
+                    <div className="text-xs font-bold text-right text-[#03738C]">
+                      {
+                        prd.price
+                          .toLocaleString("pt-br", {
+                            style: "currency", currency: "BRL"
+                          })
+                      }
+                    </div>
+                    <button className="bg-[#03738C] text-white flex items-center justify-center rounded-md p-2 cursor-pointer">
+                      <FaCartPlus size={10}/>
+                    </button>
+                  </div>
+                </Card>
+              </li>
+            ))
+          }
+        </ul>
+      </section>
+    </>
   );
 }
---
+++ app/mocks/products.ts
@@ -0,0 +1,66 @@
+export const PRODUCTS: Array<IProduct> = [
+    {
+        id: 1,
+        categoryID: 1,
+        name: "Produto 01",
+        description: "Descrição 01",
+        sellerID: 1,
+        price: 25.55
+    },
+    {
+        id: 2,
+        categoryID: 2,
+        name: "Produto 02",
+        description: "Descrição 02",
+        sellerID: 2,
+        price: 25.55
+    },
+    {
+        id: 3,
+        categoryID: 3,
+        name: "Produto 03",
+        description: "Descrição 03",
+        sellerID: 3,
+        price: 25.55
+    },
+    {
+        id: 4,
+        categoryID: 4,
+        name: "Produto 04",
+        description: "Descrição 04",
+        sellerID: 4,
+        price: 25.55
+    },
+    {
+        id: 5,
+        categoryID: 5,
+        name: "Produto 05",
+        description: "Descrição 05",
+        sellerID: 5,
+        price: 25.55
+    },
+    {
+        id: 6,
+        categoryID: 6,
+        name: "Produto 06",
+        description: "Descrição 06",
+        sellerID: 6,
+        price: 25.55
+    },
+    {
+        id: 7,
+        categoryID: 7,
+        name: "Produto 07",
+        description: "Descrição 07",
+        sellerID: 7,
+        price: 25.55
+    },
+    {
+        id: 8,
+        categoryID: 8,
+        name: "Produto 08",
+        description: "Descrição 08",
+        sellerID: 8,
+        price: 25.55
+    }
+]
